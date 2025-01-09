15a525f8823fa5fb8254af4033a41a18eea44324
Sahan Lahiru <sahanw.22@cse.mrt.ac.lk>
2025-01-09 15:17:20 +0530

    Add comments

--- src/main.js
+++ src/main.js
@@ -1,24 +1,29 @@
 import * as THREE from 'three';
 
+// Create the Scene (We include everything in it)
 const scene = new THREE.Scene();
 
+// Our object definitions
+// Shape
 const geometry = new THREE.SphereGeometry(3,32,32);
-
+//Color
 const material = new THREE.MeshStandardMaterial({
     color:'#00ff83'
 });
-
+// Combine Shape and Color
 const mesh = new THREE.Mesh(
     geometry,
     material
 );
-
+// Add to the scene
 scene.add(mesh);
 
+// Create a light to get the visibility of the object
 const light = new THREE.PointLight(0xffffff,5,100);
 light.position.set(0,5,5);
 scene.add(light);
 
+// Create a camera
 const camera = new THREE.PerspectiveCamera(
     45,
     800/600
@@ -26,6 +31,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 20
 scene.add(camera);
 
+// Create a renderer (We need a canvas to render)
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas});
 
